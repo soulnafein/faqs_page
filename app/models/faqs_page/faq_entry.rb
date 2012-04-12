8fab72528266ddfa6c5eb53db4d49974bb8ad214
David S <soulnafein@gmail.com>
2012-04-12 10:18:54 +0100

    Being more specific with the type of category

--- app/models/faqs_page/faq_entry.rb
+++ app/models/faqs_page/faq_entry.rb
@@ -1,6 +1,6 @@
 module FaqsPage
   class FaqEntry < ActiveRecord::Base
     attr_accessible :question , :answer , :position, :category
-    belongs_to :faqs_page_category
+    belongs_to :faqs_page_category, :class_name => 'FaqsPage::Category'
   end
 end
